b1692a951bda94bfa6c0193b34d429d934a74b6f
Bhaskar Gogoi <bhaskargogoi24@gmail.com>
2021-06-28 16:00:31 +0200

    Specs test 5

--- src/main/java/org/server/manufacturers/service/ManufacturerService.java
+++ src/main/java/org/server/manufacturers/service/ManufacturerService.java
@@ -115,6 +115,8 @@ public class ManufacturerService {
 
     @Transactional(propagation = Propagation.REQUIRED)
     public List<ManufacturerResponse> searchManufacturers(String searchParam) {
+/*
+
         Specification<Manufacturer> specs;
 
         if (searchParam.equalsIgnoreCase("true") || searchParam.equalsIgnoreCase("false")) {
@@ -127,8 +129,8 @@ public class ManufacturerService {
                     .or(commonSpecifications.mfrNameLike(searchParam.toLowerCase()))
                     .or(commonSpecifications.hasVehicleTypeName(searchParam.toLowerCase())));
         }
+*/
 
-        /*
         Specification<Manufacturer> specs = (searchParam.equalsIgnoreCase("true") || searchParam.equalsIgnoreCase("false")) ?
                 Specification.where(commonSpecifications.isVehicleTypePrimary(Boolean.parseBoolean(searchParam))) :
                 (searchParam.matches("\\d+")) ?
@@ -139,7 +141,6 @@ public class ManufacturerService {
                                 .or(commonSpecifications.mfrCountryLike(searchParam.toLowerCase())
                                 .or(commonSpecifications.mfrCommonNameLike(searchParam.toLowerCase()))
                                 .or(commonSpecifications.mfrNameLike(searchParam.toLowerCase()))));
-*/
 
         List<Manufacturer> manufacturers = manufacturerRepository.findAll(specs)
                 .stream().distinct().collect(Collectors.toList());
